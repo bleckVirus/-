0b0519ef109e03bd5157634599ba0612e5f588b6
bleckVirus <61931774+bleckVirus@users.noreply.github.com>
2020-06-19 07:07:01 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload= function(){
+    var  preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+};
+$(document).ready(function () {
+
+$(".newsimg").magnificPopup({
+    type:'image'
+    });
+
+    var menuButton = document.querySelector('.knp-fon');
+    var menbut = document.querySelector('.menu-button');
+    var menu = document.querySelector('.header');
+
+    menuButton.addEventListener('click', function () {
+        menbut.classList.toggle('menu-button-active');
+        menu.classList.toggle('header-active');
+    });
+
+});
